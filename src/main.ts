7cfb4d374d12a64f5f67b1e94deb197480859e88
bektidev <bekti@primaku.com>
2023-11-11 14:08:33 +0700

    add logger service port

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { env } from 'process';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv'
+import { Logger } from '@nestjs/common';
 dotenv.config()
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(env.APP_PORT);
+  Logger.log(`Service Running on port ${env.APP_PORT}`, "App")
 }
 bootstrap();
